12835c88c53ce765c4d10ab36c86e5bd29cb8f53
Thiago Gualberto <thiago.gou@gmail.com>
2020-06-04 09:12:16 -0300

    Abstraction of the contents of the routes.ts file in controllers

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,46 +1,16 @@
 import express from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/PointsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
-  
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  });
-   
-  return response.json(serializedItems);  
-});
+routes.get('/items', itemsController.create);
 
-routes.post('/points', async (request, response) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city, 
-    uf,
-    items
-  } = request.body;
-
-  await knex('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city, 
-    uf
-  });
-
-  return response.json({success: true});
-});
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
 
 export default routes;
