8d406985ca251f4d4e0fb7292b16d1e4da84d8b7
Thiago Gualberto <thiago.gou@gmail.com>
2020-06-04 11:26:34 -0300

    Creation and stylization of the home page

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,17 +1,15 @@
+/**
+ * Arquivo principal da aplicação.
+ * 1º arquivo carregado pelo React
+ */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+//Rederiza o App dentro do id 'root'
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
